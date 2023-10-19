e1086f56b120b991e671bfc8c2b32f2f87b9b0a4
Niranjan <janginmath@gmail.com>
2023-10-19 16:32:31 +0530

    python code

---
+++ github_actions_project/main.py
@@ -0,0 +1,1 @@
+print("Hello world This is the github action project")
---
+++ helloworld.py
@@ -0,0 +1,20 @@
+print ("hello world")
+
+print ('Welcome to python')
+
+print ("hello, \nniranjan")
+
+print ("Hello," "Future, python, programmer!")
+
+print ("h" + "t")
+
+print("hello", "hi", sep="-", end="*")
+
+print("hello", "hi", sep="-", end="*" "\n")
+
+print( 1, 2, 3, 4, sep='#', end='&')
+
+#print ('My age is' +25)
+
+
+
